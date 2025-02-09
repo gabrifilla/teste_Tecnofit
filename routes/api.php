8611b06c4a9ad02cc9cac9b1e07712ca2ea3813c
Gabriel Filla Camargo <gabrifillac@gmail.com>
2025-02-09 18:05:41 -0300

    Enviando rotas

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RankingController;
 use App\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/login', [AuthController::class, 'loginApi'])->name('api.login');
 Route::post('/register', [AuthController::class, 'register'])->name('api.register');
 Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('api.reset-password');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/ranking/{movementId}', [RankingController::class, 'ranking'])->name('api.ranking');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
+    Route::post('/logout', [AuthController::class, 'logoutApi'])->name('api.logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\RankingController;
 
-// Página de login
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::get('/', function () {
+    return Auth::check() ? redirect()->route('records.index', ['movementId' => 1]) : redirect()->route('login');
+});
 
-// Registro de usuário
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'loginWeb'])->name('login.submit');
+Route::post('/logout', [AuthController::class, 'logoutWeb'])->middleware('auth')->name('logout');
+
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 
-// Logout (necessário estar autenticado)
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
-
-// Reset de senha
 Route::get('/reset-password', [AuthController::class, 'showResetPassword'])->name('password.request');
 Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
 
-// Página protegida de records (somente usuários logados)
 Route::middleware('auth')->get('/records', [RankingController::class, 'showRecords'])->name('records.index');
